b7932ed187d63fb953f751bba48ba3e9daf42287
Fernanda Taborda <carneirotaborda@hotmail.com>
2024-08-14 14:22:40 +0100

    user update scenario added

--- cypress/e2e/myInfoAlterations.spec.cy.js
+++ cypress/e2e/myInfoAlterations.spec.cy.js
@@ -8,11 +8,16 @@ import userData from '../fixtures/userData.json'
     firstNameField: "[name='firstName']",
     middleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
-    employerID: ":nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
-    driveLicenseNumber: ":nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
-    licenseExpiryDate: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
+    employerIDField: ":nth-child(1) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
+    driveLicenseNumberField: ":nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input",
+    licenseExpiryDateField: ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
+    closeCalendar: ".--close",
+    dropDownTables: "[class='oxd-select-text--after']",
+    dateOfBirth: ":nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-date-wrapper > .oxd-date-input > .oxd-input",
+    genderField: "[class='--gender-grouped-field']",
+    saveButton: ":nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button"
   }
- //Data alteration in my info -- Alteração de dados em My info
+
  it('My info - alteration', () => {
     //login
     cy.visit('/auth/login')
@@ -20,11 +25,25 @@ import userData from '../fixtures/userData.json'
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.get(selectorsList.myInfoButton).click()
-    //Data alteration in myInfo -- Alteração de dados em myInfo
+    //Changing data in myInfo -- Alteração de dados em myInfo
     cy.get(selectorsList.firstNameField).clear().type(userData.personalDetails.firstName)
     cy.get(selectorsList.middleNameField).clear().type(userData.personalDetails.middleName)
     cy.get(selectorsList.lastNameField).clear().type(userData.personalDetails.lastName)
-    cy.get(selectorsList.employerID).clear().type(userData.personalDetails.employerID)
-    cy.get(selectorsList.driveLicenseNumber).clear().type(userData.personalDetails.driveLicenseNumber)
-    cy.get(selectorsList.licenseExpiryDate).clear().type(userData.personalDetails.licenseExpiryDate)  
+    cy.get(selectorsList.employerIDField).clear().type(userData.personalDetails.employerID)
+    cy.get(selectorsList.driveLicenseNumberField).clear().type(userData.personalDetails.driveLicenseNumber)
+    cy.get(selectorsList.licenseExpiryDateField).clear().type(userData.personalDetails.licenseExpiryDate)  
+    cy.get(selectorsList.closeCalendar).click()
+
+    cy.get(selectorsList.dropDownTables).eq(0).should('be.visible').click()
+    cy.get('.oxd-select-dropdown').contains(userData.personalDetails.nationality).click()
+
+    cy.get(selectorsList.dropDownTables).eq(1).click()
+    cy.get('.oxd-select-dropdown').contains(userData.personalDetails.maritalSatatus).click()
+
+    cy.get(selectorsList.dateOfBirth).clear().type(userData.personalDetails.dateOfBirth)
+    cy.get(selectorsList.closeCalendar).click()
+
+    cy.get(selectorsList.genderField).contains(userData.personalDetails.gender).click()
+
+    //cy.get(selectorsList.saveButton).click()
   })
